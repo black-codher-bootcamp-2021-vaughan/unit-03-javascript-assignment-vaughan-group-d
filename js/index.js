d27f809c41508b74d849540d6af1f2a315f396d5
karacodes88 <karawhite59@gmail.com>
2022-01-23 16:11:26 +0000

    new update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 import {dates} from './data.js';
 console.log(dates);
 
+
 dates.map((date) => {
     console.log(date)
     const timelineItemDiv = document.createElement("div");
     timelineItemDiv.classList.add("timeline-item");
-    timelineContainer.appendChild(timelineItemDiv);
+    const timelineItem = document.createElement("div");
   });
+
   
+
+  
+
 
   
