a71ab4c216c6e0ad8a97fd61c3f6bfce00f455da
karacodes88 <karawhite59@gmail.com>
2022-01-23 21:15:06 +0000

    new changes

--- js/index.js
+++ js/index.js
@@ -7,10 +7,25 @@ dates.map((date) => {
     const timelineItemDiv = document.createElement("div");
     timelineItemDiv.classList.add("timeline-item");
     const timelineItem = document.createElement("div");
+    const timelineItemTitle = document.createElement("h2");
+    const title = document.createTextNode("date,title"); 
+    timelineItemTitle.appendChild(title);
+    timelineItem.appendChild(timelineItemTitle);
+    timelineItemTitle.className = "timeline-item-title"
+    const timelineItemDate = document.createElement("span");
   });
 
-  
 
+//  trying to make a button 
+  const button = document.createElement("button");
+  const content = document.createTextNode(
+      "More Info "
+  );
+  
+  button.appendChild(content);
+  document.body.appendChild(button);
+  button.onclick = summary;
+  
   
 
 
