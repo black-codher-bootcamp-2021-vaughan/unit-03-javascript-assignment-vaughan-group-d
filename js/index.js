a403b11112c341a7349e7ecd4077e94d3d501e8a
kara <karawhite59@gmail.com>
2022-01-25 20:58:03 +0000

    new changes

--- js/index.js
+++ js/index.js
@@ -6,25 +6,36 @@ dates.map((date) => {
     console.log(date)
     const timelineItemDiv = document.createElement("div");
     timelineItemDiv.classList.add("timeline-item");
-    const timelineItem = document.createElement("div");
+
     const timelineItemTitle = document.createElement("h2");
-    const title = document.createTextNode("date,title"); 
-    timelineItemTitle.appendChild(title);
-    timelineItem.appendChild(timelineItemTitle);
     timelineItemTitle.className = "timeline-item-title"
+    timelineItemTitle.textContent = date.title;
+    document.body.prepend(timelineItemTitle);
+    
+    
     const timelineItemDate = document.createElement("span");
   });
+// // 1. Create the element 
+// const timelineItemTitle = document.createElement("h2");
 
+// // 2. Give it a class name 
+// timelineItemTitle.classList.add("timeline-item-title");
+
+//   // 3. Give it text 
+// timelineItemTitle.textContent = date.title; 
+
+// // 4. add to the DOM (browser window )
+// document.body.appendChild(timelineItemTitle);
 
 //  trying to make a button 
-  const button = document.createElement("button");
-  const content = document.createTextNode(
-      "More Info "
-  );
+  // const button = document.createElement("button");
+  // const content = document.createTextNode(
+  //     "More Info "
+  // );
   
-  button.appendChild(content);
-  document.body.appendChild(button);
-  button.onclick = summary;
+  // button.appendChild(content);
+  // document.body.appendChild(button);
+  // button.onclick = summary;
   
   
 
